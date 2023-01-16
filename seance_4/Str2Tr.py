8b605230ecc4ade8654b54e42392c15b1acbdc60
DelphineFqt1 <91529341+DelphineFqt1@users.noreply.github.com>
2023-01-16 15:35:36 +0100

    Update Str2Tr

--- seance_4/Str2Tr.py
+++ seance_4/Str2Tr.py
@@ -1,6 +1,15 @@
 from seance_2.TransitionRelation import TransitionRelation
 
 class Str2Tr(TransitionRelation) :
-    def __init__(self, roots, next):
-        self.roots = roots
-        self.next = next
+    def __init__(self, str):
+        self.str = str
+
+    def roots(self):
+        return self.str.initialConfigurations()
+
+    def next(self, source):
+        Actions = self.str.enabledActions(source)
+        r = []
+        for action in Actions:
+            r.append(self.str.execute(action, source))
+        return r
